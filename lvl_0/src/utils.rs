9537e978bba1d03beb12c5845ce68cc3df6ded06
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-07-11 20:51:50 +0200

    fix utils

--- lvl_0/src/utils.rs
+++ lvl_0/src/utils.rs
@@ -1,11 +1,11 @@
-use quad_url::{get_program_parameters};
+use quad_url::get_program_parameters;
 
 
 pub fn get_parameter_value(arg: &str) -> String {
     let parameters = get_program_parameters();
     for parameter in parameters.iter() {
         if parameter.starts_with("--") {
-            let para = &parameter[2..];
+            let para = parameter.strip_prefix("--").unwrap();
             let para = para.to_string();
             let para2: Vec<&str> = para.split('=').collect();
             if para2.len() == 2 && para2[0] == arg {
